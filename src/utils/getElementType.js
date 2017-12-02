b25c76f29e35bc1d31119b9be756e0091c4710a7
Jon Saw <jon.saw@gmail.com>
2017-12-03 00:16:04 +0800

    get element type utility

---
+++ src/utils/getElementType.js
@@ -0,0 +1,15 @@
+
+export default function getElementType(Component, props, getDefault) {
+  const { defaultProps = {} } = Component;
+
+  if (props.as && props.as !== defaultProps.as) return props.as;
+
+  if (getDefault) {
+    const computedDefault = getDefault();
+    if (computedDefault) return computedDefault;
+  }
+
+  if (props.href) return 'a';
+
+  return defaultProps.as || 'div';
+}
