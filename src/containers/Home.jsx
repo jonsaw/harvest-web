0eff3ddd36d7b2a6254bce5a75d71a2823b7e718
Jon Saw <jon.saw@gmail.com>
2017-12-03 18:04:43 +0800

    show services on home screen

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,6 +7,24 @@ import banner from '../assets/images/banner.png';
 
 const classNames = (...names) => names.filter(n => !!n).join(' ');
 
+const services = [
+  {
+    name: 'English Service',
+    time: 'Sundays at 10:30am',
+    location: 'Main Sanctuary',
+  },
+  {
+    name: 'Tamil Service',
+    time: 'Sundays at 11:30am',
+    location: 'Rooftop Sanctuary',
+  },
+  {
+    name: 'Chinese Service',
+    time: 'Saturdays at 8:00pm',
+    location: 'Rooftop Sanctuary',
+  },
+];
+
 const Home = ({
   className,
 }) => (
@@ -26,6 +44,21 @@ const Home = ({
     </nav>
     <div className="banner">
       <img alt="Banner" src={banner} />
+    </div>
+    <div className="services">
+      <ul>
+        {
+          services.map(service => (
+            <li key={service.name} className="service">
+              <h5>{service.name}</h5>
+              <ul>
+                <li>{service.time}</li>
+                <li>{service.location}</li>
+              </ul>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   </div>
 );
