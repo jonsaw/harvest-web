d0a4f02fc6e4e4d2ac4b54d3d5165c64ab77ec58
Jon Saw <jon.saw@gmail.com>
2017-12-10 00:43:10 +0800

    newsletter endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,6 @@
 import config from './config';
 
 export const EVENTS = `${config.apiURL}/events`;
+export const NEWSLETTERS = `${config.apiURL}/newsletters`;
+export const NEWSLETTER = id => `${config.apiURL}/newsletters/${id}`;
 export const STAFF = `${config.apiURL}/staff`;
